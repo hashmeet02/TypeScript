99785d33062564135cce060f7fa73697aca3d3e5
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2025-01-21 15:28:02 -0500

    features changed

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+let user = [1, "Mosh"];
+user.push(1);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-let sales: number=123_456789;
+let sales: number=123_456_789; //_ helps to simply reading large numbers.
+let course = "TypeScript";  // TS compiler can also infer types. For ex. 
+                            // here course is a string variable even 
+                            // though not explicity annotated
+let is_published= true;
+let level;                  //If not initialized, type is any
+
+let user:[number,string]=[1,"Mosh"];
+user.push(1);
