dbda6ab96b5436ed0859ec671f943509860437bb
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2025-02-10 17:23:58 -0500

    Inheritance

---
+++ src/inheritance.ts
@@ -0,0 +1,26 @@
+class Person{
+    constructor(public firstName:string, public lastName:string){}
+    get fullName(){
+        return this.firstName + " " + this.lastName;
+    }
+    walk(){
+        console.log("Walking");
+    }
+    talk(){
+        console.log("Talking");
+    }
+}
+
+class Student extends Person{
+    constructor(
+        public studentId:number, 
+        firstName:string,
+        lastName:string){
+        super(firstName,lastName)
+    }
+    takeTest(){
+        console.log("taking a test")
+    }
+}
+
+let student=new Student(1, "John", "john@gmail.com");
